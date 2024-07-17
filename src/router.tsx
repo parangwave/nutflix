465c1151a38f8ced908b93ea99f571759c692cce
parangwave <parangwavetraveller@gmail.com>
2024-07-18 01:19:12 +0900

    feat: Add Router Setting

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,21 @@
 import App from "./App";
 import { createBrowserRouter } from "react-router-dom";
 
+// elements
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import ComingSoon from "./pages/ComingSoon";
+import NowPlaying from "./pages/NowPlaying";
+
 export const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    path: "/",
+    errorElement: <Error />,
+    children: [
+      { element: <Home />, path: "" },
+      { element: <ComingSoon />, path: "coming-soon" },
+      { element: <NowPlaying />, path: "now-playing" },
+    ],
   },
 ]);
