13e0e4060ce0925c8db4cd470edd6527d0b516e9
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-22 03:45:48 +0900

    feat: Add MovieModal in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
+// components
+import MovieModal from "./components/MovieModal";
+import Header from "./components/Header";
+
 export default function App() {
+  const [selectedMovie, setSelectedMovie] = useState<string | null>(null);
   return (
     <div>
-      <Outlet />
+      <Header toggleTheme={toggleTheme} />
+      <Outlet context={[selectedMovie, setSelectedMovie]} />
+      {selectedMovie && (
+        <MovieModal id={selectedMovie} onClose={() => setSelectedMovie(null)} />
+      )}
     </div>
   );
 }
