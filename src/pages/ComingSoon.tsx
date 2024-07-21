fa1c270eecf0c19f307792baf877982f63a43b93
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-22 03:57:49 +0900

    feat: Add MovieList in ComingSoon

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+// api
+import { getComingSoon } from "../utils/api";
+
+// components
+import MovieList from "../components/MovieList";
+import MovieModal from "../components/MovieModal";
+
 export default function ComingSoon() {
-  return <h1>Coming Soon</h1>;
+  const { data, error, isLoading } = useQuery("comingSoon", getComingSoon);
+  const [selectedMovie, setSelectedMovie] = useState<string | null>(null);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data.</div>;
+
+  return (
+    <div>
+      <MovieList movies={data.results} onMovieClick={setSelectedMovie} />
+      {selectedMovie && (
+        <MovieModal id={selectedMovie} onClose={() => setSelectedMovie(null)} />
+      )}
+    </div>
+  );
 }
