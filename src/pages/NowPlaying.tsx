208e273967dfb734e8afe5a30611e3cbf97459dd
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-22 03:57:09 +0900

    feat: Add MovieList in NowPlaying

--- src/pages/NowPlaying.tsx
+++ src/pages/NowPlaying.tsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+// api
+import { getNowPlaying } from "../utils/api";
+
+// components
+import MovieList from "../components/MovieList";
+import MovieModal from "../components/MovieModal";
+
 export default function NowPlaying() {
-  return <h1>Now Playing</h1>;
+  const { data, error, isLoading } = useQuery("nowPlaying", getNowPlaying);
+  const [selectedMovie, setSelectedMovie] = useState<string | null>(null);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data.</div>;
+
+  return (
+    <div>
+      <MovieList movies={data.results} onMovieClick={setSelectedMovie} />
+      {selectedMovie && (
+        <MovieModal id={selectedMovie} onClose={() => setSelectedMovie(null)} />
+      )}
+    </div>
+  );
 }
