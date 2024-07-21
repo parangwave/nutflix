86a88bf95b99c58879f7741b099351ca0071c916
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-22 03:57:22 +0900

    feat: Modify Error

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,3 +1,14 @@
+import styled from "styled-components";
+
+const ErrorHeader = styled.div`
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
 export default function Error() {
-  return <h1>Error</h1>;
+  return <ErrorHeader>Error</ErrorHeader>;
 }
