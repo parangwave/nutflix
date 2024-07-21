0d20c97643a26ca2f319869bc46c06152f80d4cb
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-22 04:00:36 +0900

    feat: Add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+// types
+import { HeaderProps } from "../utils/types";
+
+const HeaderContainer = styled.header`
+  width: 100vw;
+  padding: 2rem;
+  background-color: ${(props) => props.theme.bg};
+  color: ${(props) => props.theme.text};
+  box-shadow: ${(props) => props.theme.shadow};
+`;
+
+const Nav = styled.nav`
+  width: 100vw;
+  position: relative;
+  display: flex;
+  justify-content: space-around;
+  padding: 0 4rem;
+  gap: 2rem;
+`;
+
+const NavItem = styled(Link)<{ isActive: boolean }>`
+  position: relative;
+  text-decoration: none;
+  font-size: 1.2rem;
+  font-weight: ${(props) => (props.isActive ? "bold" : "normal")};
+  color: ${(props) => props.theme.text};
+  text-align: center;
+
+  &:hover {
+    font-weight: bold;
+  }
+`;
+
+const Header: React.FC<HeaderProps> = ({ toggleTheme }) => {
+  const location = useLocation();
+  const [selectedTab, setSelectedTab] = useState(location.pathname);
+
+  return (
+    <HeaderContainer>
+      <Nav>
+        <NavItem
+          to="/"
+          isActive={location.pathname === "/"}
+          onClick={() => setSelectedTab("/")}
+        >
+          Home
+        </NavItem>
+        <NavItem
+          to="/coming-soon"
+          isActive={location.pathname === "/coming-soon"}
+          onClick={() => setSelectedTab("/coming-soon")}
+        >
+          Coming Soon
+        </NavItem>
+        <NavItem
+          to="/now-playing"
+          isActive={location.pathname === "/now-playing"}
+          onClick={() => setSelectedTab("/now-playing")}
+        >
+          Now Playing
+        </NavItem>
+      </Nav>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
