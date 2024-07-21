979a2149a2943a5de65ea1aa2ca301d296b9dca9
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-22 03:55:53 +0900

    feat: Add MovieList in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,27 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
+
+// api
+import { getPopular } from "../utils/api";
+
+// components
+import MovieList from "../components/MovieList";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const { data, error, isLoading } = useQuery("popular", getPopular);
+  const [selectedMovie, setSelectedMovie] =
+    useOutletContext<
+      [string | null, React.Dispatch<React.SetStateAction<string | null>>]
+    >();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data.</div>;
+
+  return (
+    <div>
+      {/* popular movies */}
+      <MovieList movies={data.results} onMovieClick={setSelectedMovie} />
+    </div>
+  );
 }
